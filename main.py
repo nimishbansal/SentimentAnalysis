0180eb0eb4fcbe0b6c8b657cd4ab4ccb11537d37
nimish <nimish4july1998@gmail.com>
2019-09-29 20:38:26 +0530

    cleaned code and change some features

--- main.py
+++ main.py
@@ -6,22 +6,8 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import accuracy_score
 
-df = pd.read_excel('main_dataset_2.xlsx', encoding='ISO-8859-1')
-X_data = df[['Phrase']].values
-Y_data = df[['Sentiment']].values
-# The sentiment labels are:
-# 0 - negative
-# 1 - somewhat negative
-# 2 - neutral
-# 3 - somewhat positive
-# 4 - positive
 
-print(df.head())
-
-processed_features = []
-
-for sentence in range(0, len(X_data)):
-    # Remove all the special characters
+def process_feature(sentence):
     processed_feature = re.sub(r'\W', ' ', str(X_data[sentence][0]))
 
     # remove all single characters
@@ -39,13 +25,30 @@ for sentence in range(0, len(X_data)):
     # Converting to Lowercase
     processed_feature = processed_feature.lower()
 
+    return processed_feature
+
+
+df = pd.read_excel('main_dataset_2.xlsx', encoding='ISO-8859-1')
+X_data = df[['Phrase']].values
+Y_data = df[['Sentiment']].values
+# The sentiment labels are:
+# 0 - negative
+# 2 - neutral
+# 4 - positive
+
+print(df.head())
+
+processed_features = []
+
+for sentence in range(0, len(X_data)):
+    # Remove all the special characters
+    processed_feature = process_feature(sentence)
     processed_features.append(processed_feature)
 
-
-vectorizer = TfidfVectorizer(max_features=10, min_df=7, max_df=0.8, stop_words=stopwords.words('english'))
+vectorizer = TfidfVectorizer(max_features=1, min_df=15, max_df=0.9, stop_words=stopwords.words('english'))
 processed_features = vectorizer.fit_transform(processed_features).toarray()
 
-X_train, X_test, y_train, y_test = train_test_split(processed_features, Y_data, train_size=0.5)
+X_train, X_test, y_train, y_test = train_test_split(processed_features, Y_data, train_size=0.7)
 classfier = GaussianNB()
 classfier.fit(X_train, y_train)
 y_pred = classfier.predict(X_test)
